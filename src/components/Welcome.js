95dbf4b82da1444721b00da22713e426e3c89c7e
me222wm <me222wm@student.lnu.se>
2016-08-01 02:05:40 +0200

    Nice welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
+import '../styles/welcome.css';
+import { PATHS } from '../constants';
 
-export default () => <div>Welcome to our slice of paradise!</div>;
+const Welcome = () => (
+  <div className="ui center aligned segment">
+    <div className="ui text container">
+      <h1>Välkommen till OrdoCliens</h1>
+      <p>OrdoCliens är ett enkelt system för små juristfirmor.</p>
+      <Link className="ui huge primary button" to={PATHS.signUp}>Registera mig nu!</Link>
+    </div>
+  </div>
+);
+
+export default Welcome;
