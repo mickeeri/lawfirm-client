2622d528e37a60ddae20b9d8a1e183fea5d8235e
me222wm <me222wm@student.lnu.se>
2016-08-01 23:37:23 +0200

    Client info

---
+++ src/components/clients/ClientInfo.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { PATHS } from '../../constants';
+
+const ClientInfo = ({ client }) => {
+  return (
+    <div className="ui segment">
+      <h2 className="ui header">{client.first_name} {client.last_name}</h2>
+      <div className="ui divider"></div>
+      <p>Personnummer: {client.personal_number}</p>
+      <p>E-post:</p>
+      <p>Mobil:</p>
+      <p>Telefon:</p>
+      <div className="ui section divider"></div>
+
+      <h3 className="ui header">Adress</h3>
+      <p>C/O</p>
+      <p>Gatuadress</p>
+      <p>Posadress</p>
+      <p>Ort</p>
+      <div className="ui section divider"></div>
+
+      <h3 className="ui header">Anteckningar</h3>
+      <p>Lorem ipusm...</p>
+      <div className="ui section divider"></div>
+
+      <h3 className="ui header">Inställningar</h3>
+      <button className="negative labeled icon ui button">
+        <i className="remove user icon"></i>Radera klient
+      </button>
+      <button className="ui primary labeled icon button">
+        <i className="edit icon"></i>Redigera
+      </button>
+      <div className="ui section divider"></div>
+      <Link to={PATHS.clients} className="ui small labeled icon button">
+        <i className="left chevron icon"></i>Tillbaka till klientregister
+      </Link>
+    </div>
+  );
+};
+
+ClientInfo.propTypes = {
+  client: PropTypes.object.isRequired,
+};
+
+export default ClientInfo;
