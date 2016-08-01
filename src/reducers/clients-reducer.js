f4bbf97dd0ca7a08e91de59c33898c23f9badf2d
me222wm <me222wm@student.lnu.se>
2016-08-01 02:07:28 +0200

    Meta params to clients reducer

--- src/reducers/clients-reducer.js
+++ src/reducers/clients-reducer.js
@@ -1,12 +1,12 @@
 import { FETCH_CLIENTS } from '../actions/types';
 
-const clientsReducer = (state = { all: [], client: null }, action ) => {
+const clientsReducer = (state = { all: [], client: null, meta: {} }, action) => {
   switch (action.type) {
     case FETCH_CLIENTS:
-      return { ...state, all: action.payload };
+      return { ...state, all: action.payload.clients, meta: action.payload.meta };
     default:
       return state;
   }
-}
+};
 
 export default clientsReducer;
