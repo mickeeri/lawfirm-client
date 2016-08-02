4a500648ef6bcafbdc7b1e1b6b78f8d529e4a61f
me222wm <me222wm@student.lnu.se>
2016-08-03 01:24:16 +0200

    Update lawsuit reducer

--- src/reducers/lawsuitsReducer.js
+++ src/reducers/lawsuitsReducer.js
@@ -1,11 +1,12 @@
 import { FETCH_LAWSUITS, FETCH_LAWSUIT } from '../actions/types';
 
-const lawsuitsReducer = (state = { all: [], lawsuit: null, meta: {} }, action) => {
+const lawsuitsReducer = (state = { all: [], lawsuit: null, meta: {}, filter: { query: '', page: 1, status: 'only_active'} }, action) => {
   switch (action.type) {
     case FETCH_LAWSUIT:
       return { ...state, lawsuit: action.payload.lawsuit };
     case FETCH_LAWSUITS:
-      return { ...state, all: action.payload.lawsuits, meta: action.payload.meta };
+      const { payload, query } = action;
+      return { ...state, all: payload.lawsuits, meta: payload.meta, filter };
     default:
       return state;
   }
