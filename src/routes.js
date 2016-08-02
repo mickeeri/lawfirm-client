d33dbb5b57ea77dbce4cdb1d8ae9980505c36bcc
me222wm <me222wm@student.lnu.se>
2016-08-03 01:23:37 +0200

    New routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import App from './components/App';
+import ClientShow from './components/clients/ClientShow';
 import ClientsIndex from './components/clients/ClientsIndex';
-import ClientsShow from './components/clients/ClientsShow';
+import LawsuitShow from './components/lawsuits/LawsuitShow';
 import LawsuitsIndex from './components/lawsuits/LawsuitsIndex';
 import React from 'react';
 import requireAuth from './components/auth/requireAuth';
@@ -16,7 +17,8 @@ export default (
     <Route path={PATHS.signIn} components={SignInPage} />
     <Route path={PATHS.signOut} component={SignOut} />
     <Route path={PATHS.clients} component={requireAuth(ClientsIndex)} />
-    <Route path={`${PATHS.client}/:id`} component={requireAuth(ClientsShow)} />
+    <Route path={`${PATHS.client}/:id`} component={requireAuth(ClientShow)} />
     <Route path={PATHS.lawsuits} component={requireAuth(LawsuitsIndex)} />
+    <Route path={`${PATHS.lawsuit}/:id`} component={requireAuth(LawsuitShow)} />
   </Route>
 );
