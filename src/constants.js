eb8bbf19c39cf536e1b9862bab94dd9f197ef9d4
me222wm <me222wm@student.lnu.se>
2016-08-03 14:33:28 +0200

    localStorage keys as constants

--- src/constants.js
+++ src/constants.js
@@ -13,3 +13,6 @@ export const MESSAGES = {
   emailRequired: 'E-post måste anges',
   passwordRequired: 'Lösenord måste anges',
 };
+
+export const USER_ID_LS_KEY = 'signed_in_user_id';
+export const AUTH_TOKEN_LS_KEY = 'auth_token';
