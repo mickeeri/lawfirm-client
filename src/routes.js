9d7e22822f9c6012ee261dee3a1ee9b20c70edee
me222wm <me222wm@student.lnu.se>
2016-08-06 12:52:42 +0200

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Welcome from './components/Welcome';
 import { Route, IndexRoute } from 'react-router';
 import { USER_SIGNIN_PATH, USER_SIGNOUT_PATH, UserSignIn, UserSignOut } from './users';
-import { LawsuitsIndexRoute, LawsuitShowRoute } from './lawsuits';
+import { LAWSUITS_PATH, LawsuitsIndex, LawsuitShow } from './lawsuits';
 import { ClientShow, ClientsIndex, CLIENTS_PATH } from './clients';
 import requireAuth from './shared/hocs/requireAuth';
 
@@ -12,16 +12,9 @@ export default (
     <IndexRoute components={Welcome} />
     <Route path={USER_SIGNIN_PATH} components={UserSignIn} />
     <Route path={USER_SIGNOUT_PATH} components={UserSignOut} />
-    {LawsuitsIndexRoute}
-    {LawsuitShowRoute}
+    <Route path={LAWSUITS_PATH} components={requireAuth(LawsuitsIndex)} />
+    <Route path={`${LAWSUITS_PATH}/:id`} component={requireAuth(LawsuitShow)} />
     <Route path={CLIENTS_PATH} components={requireAuth(ClientsIndex)} />
     <Route path={`${CLIENTS_PATH}/:id`} components={requireAuth(ClientShow)} />
   </Route>
 );
-
-//  <Route path={PATHS.signOut} component={SignOut} />
-// <Route path={PATHS.userSignUp} component={UserSignUp} />
-// <Route path={PATHS.clients} component={requireAuth(ClientsIndex)} />
-// <Route path={`${PATHS.client}/:id`} component={requireAuth(ClientShow)} />
-// <Route path={PATHS.lawsuits} component={requireAuth(LawsuitsIndex)} />
-// <Route path={`${PATHS.lawsuit}/:id`} component={requireAuth(LawsuitShow)} />
