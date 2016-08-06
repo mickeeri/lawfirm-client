17fac44a76fc175fe23488487d60258c4effb638
me222wm <me222wm@student.lnu.se>
2016-08-06 12:54:50 +0200

    Remove unused files

--- src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_ERROR = 'auth_error';
-export const FETCH_CLIENT = 'fetch_client';
-export const FETCH_CLIENTS = 'fetch_clients';
-export const FETCH_LAWSUITS = 'fetch_lawsuits';
-export const FETCH_LAWSUIT = 'fetch_lawsuit';
-
