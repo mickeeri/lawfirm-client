1b3e145ec6f0f7d16392366eda21df8643e3d061
me222wm <me222wm@student.lnu.se>
2016-08-08 15:15:50 +0200

    Update production api url

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 export const AUTH_TOKEN_LS_KEY = 'auth_token';
-export const API_ROOT_URL = process.env.NODE_ENV === 'production' ? 'http://lawfirm-api-dev.eu-central-1.elasticbeanstalk.com/v1' : 'http://localhost:3090/v1';
+export const API_ROOT_URL = process.env.NODE_ENV === 'production' ? 'https://www.mikaeleriksson.xyz/v1' : 'http://localhost:3090/v1';
 
 // Messages
 export const FIRST_NAME_REQ_MESSAGE = 'Förnamn måste anges';
