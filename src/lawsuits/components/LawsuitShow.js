3a28c6e97e98d37a8ed1cb9321ff68169730c6dd
me222wm <me222wm@student.lnu.se>
2016-09-04 21:22:19 +0200

    Update layout lawsuit show

--- src/lawsuits/components/LawsuitShow.js
+++ src/lawsuits/components/LawsuitShow.js
@@ -39,81 +39,107 @@ class LawsuitShow extends Component {
     }
 
     return (
-      <div className="show-grid">
-        <div className="column">
-          <div className="segment">
-            <div className="lawsuit-show-header">
-              <h2>Ärende {lawsuit.slug}</h2>
-              { lawsuit.closed &&
-                <span className="archived"><Icon name="archive" />Arkiverat</span> }
+      <div>
+        <h2>Ärende {lawsuit.slug}</h2>
+        <div className="tabbed-area">
+          <ul className="tabs group">
+            <li><a href="#" className="active">Översikt</a></li>
+            <li><a href="#">Arbete</a></li>
+            <li><a href="#">Utlägg</a></li>
+          </ul>
+
+          <div className="tabbed segment">
+            <div className="show-grid">
+              <div className="column">
+                <div className="card">
+                  <div className="lawsuit-show-header card-header">
+                    <h3>Uppgifter</h3>
+                    { lawsuit.closed &&
+                      <span className="archived"><Icon name="archive" />Arkiverat</span> }
+                  </div>
+                  <div className="card-content">
+                    <a className="edit-link" onClick={toggleEdit}>Ändra</a>
+
+                    {/* Show form or info based on bool edit */}
+                    {edit ?
+                      <LawsuitForm
+                        onSubmit={createUpdateLawsuit}
+                        errorMessage={errorMessage}
+                        toggleEdit={toggleEdit}
+                        primaryClientId={lawsuit.primary_client.id}
+                      /> :
+                      <LawsuitInfo lawsuit={lawsuit} />
+                    }
+
+                    <div className="ui divider" />
+
+                    <a>Redigera anteckning</a>
+                    <h3>Anteckning</h3>
+                    <p>{lawsuit.note}</p>
+
+                    <div className="ui section divider" />
+
+                    <h3>Inställningar</h3>
+
+                    <div className="settings">
+                      <LawsuitDeleteButton />
+
+                      <LawsuitArchiveButton
+                        toggleClosed={() => {
+                          createUpdateLawsuit({ id: lawsuit.id, closed: !lawsuit.closed });
+                        }}
+                        closed={lawsuit.closed}
+                      />
+                    </div>
+
+                    <div className="ui section divider" />
+
+                    <Link to={LAWSUITS_PATH}>
+                      <Icon name="chevron-left" />Tillbaka till ärendelista
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <ClientsList
+                  openClientFormModal={() => { dispatch(openDialog(CLIENT_FORM_MODAL_NAME)); }}
+                  openClientsDropdownModal={
+                    () => { dispatch(openDialog(CLIENTS_DROPDOWN_MODAL_NAME)); }
+                  }
+                  clients={lawsuit.clients}
+                  primaryClientId={lawsuit.primary_client.id}
+                />
+
+                <CounterpartsList
+                  counterparts={lawsuit.counterparts}
+                  openDropdownModal={
+                    () => { dispatch(openDialog(COUNTERPARTS_DROPDOWN_MODAL_NAME)); }
+                  }
+                  openFormModal={
+                    () => { dispatch(openDialog(COUNTERPARTS_FORM_MODAL_NAME)); }
+                  }
+                />
+
+                <table className="ui celled table">
+                  <thead>
+                    <tr>
+                      <th>Datum</th>
+                      <th>Notering</th>
+                      <th>Arbetat tid</th>
+                      <th>Kategori</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>2016-07-11</td>
+                      <td>Nasdlkjasdjsadlasjdasld</td>
+                      <td>2,5 h</td>
+                      <td>Tidssppillan (låg)</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-
-            <div className="ui divider" />
-
-            <a onClick={toggleEdit}>Ändra</a>
-
-            {/* Show form or info based on bool edit */}
-            {edit ?
-              <LawsuitForm
-                onSubmit={createUpdateLawsuit}
-                errorMessage={errorMessage}
-                toggleEdit={toggleEdit}
-                primaryClientId={lawsuit.primary_client.id}
-              /> :
-              <LawsuitInfo lawsuit={lawsuit} />
-            }
-
-            <div className="ui divider" />
-
-            <a>Redigera anteckning</a>
-            <h3>Anteckning</h3>
-            <p>{lawsuit.note}</p>
-
-            <div className="ui section divider" />
-
-            <h3>Inställningar</h3>
-
-            <div className="settings">
-              <LawsuitDeleteButton />
-
-              <LawsuitArchiveButton
-                toggleClosed={() => {
-                  createUpdateLawsuit({ id: lawsuit.id, closed: !lawsuit.closed });
-                }}
-                closed={lawsuit.closed}
-              />
-            </div>
-
-            <div className="ui section divider" />
-
-            <Link to={LAWSUITS_PATH}>
-              <Icon name="chevron-left" />Tillbaka till ärendelista
-            </Link>
-          </div>
-        </div>
-        <div className="column">
-
-          <div className="segment">
-            <ClientsList
-              openClientFormModal={() => { dispatch(openDialog(CLIENT_FORM_MODAL_NAME)); }}
-              openClientsDropdownModal={
-                () => { dispatch(openDialog(CLIENTS_DROPDOWN_MODAL_NAME)); }
-              }
-              clients={lawsuit.clients}
-              primaryClientId={lawsuit.primary_client.id}
-            />
-          </div>
-
-          <div className="segment">
-            <CounterpartsList
-              counterparts={lawsuit.counterparts}
-              openDropdownModal={
-                () => { dispatch(openDialog(COUNTERPARTS_DROPDOWN_MODAL_NAME)); }
-              }
-              openFormModal={
-                () => { dispatch(openDialog(COUNTERPARTS_FORM_MODAL_NAME)); }
-              }
-            />
           </div>
         </div>
       </div>
