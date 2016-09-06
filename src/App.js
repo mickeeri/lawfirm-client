3ddc929ab14c858a2d542b983e7856948684639a
me222wm <me222wm@student.lnu.se>
2016-09-06 21:07:11 +0200

    Import delete modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { PropTypes } from 'react';
 import 'semantic-ui-table/table.css';
-import 'semantic-ui-button/button.css';
 import 'semantic-ui-divider/divider.css';
 import 'semantic-ui-loader/loader.css';
 import './shared/styles/modal.css';
 import './shared/styles/header.css';
 import Header from './shared/components/Header';
 import AlertMessage from './shared/components/AlertMessage';
+import ModalRoot from './modals/components/ModalRoot';
 
 const App = (props) =>
   <div className="App">
@@ -19,6 +19,7 @@ const App = (props) =>
         {props.children}
       </div>
     </main>
+    <ModalRoot />
   </div>;
 
 App.propTypes = {
