a912d553679006b6edb7ef1f390ac39af0884b2e
me222wm <me222wm@student.lnu.se>
2016-09-06 20:25:14 +0200

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-
 import App from './App';
 import Welcome from './shared/components/Welcome';
 import {
@@ -17,24 +16,28 @@ import requireAuth from './shared/hocs/requireAuth';
 import beforeAuth from './shared/hocs/beforeAuth';
 import { COUNTERPARTS_PATH } from './counterparts/constants';
 import CounterpartShow from './counterparts/components/CounterpartShow';
+import TasksIndex from './tasks/components/TasksIndex';
+import { TASKS_PATH } from './tasks/constants';
+import LawsuitOverview from './lawsuits/components/LawsuitOverview';
 
 export default (
   <Route path="/" component={App} >
-    <IndexRoute components={Welcome} />
-    <Route path={USER_SIGNIN_PATH} components={beforeAuth(UserSignIn)} />
-    <Route path={USER_SIGNOUT_PATH} components={UserSignOut} />
-    <Route path={USER_SIGNUP_PATH} components={beforeAuth(UserSignUp)} />
+    <IndexRoute component={Welcome} />
+    <Route path={USER_SIGNIN_PATH} component={beforeAuth(UserSignIn)} />
+    <Route path={USER_SIGNOUT_PATH} component={UserSignOut} />
+    <Route path={USER_SIGNUP_PATH} component={beforeAuth(UserSignUp)} />
 
-    <Route path={`${COUNTERPARTS_PATH}/:id`} components={requireAuth(CounterpartShow)} />
+    <Route path={`${COUNTERPARTS_PATH}/:id`} component={requireAuth(CounterpartShow)} />
 
-    <Route path={LAWSUITS_PATH} components={requireAuth(LawsuitsIndex)} />
-    <Route path={`${LAWSUITS_PATH}/:id`} component={requireAuth(LawsuitShow)} />
-    <Route path={COI_SEARCH_PATH} components={requireAuth(COISearch)} />
+    <Route path={LAWSUITS_PATH} component={requireAuth(LawsuitsIndex)} />
+    <Route path={`${LAWSUITS_PATH}/:id`} component={requireAuth(LawsuitShow)}>
+      <IndexRoute component={LawsuitOverview} />
+      <Route path={`${LAWSUITS_PATH}/:id${TASKS_PATH}`} component={requireAuth(TasksIndex)} />
+    </Route>
+    <Route path={COI_SEARCH_PATH} component={requireAuth(COISearch)} />
 
-    <Route path={CLIENT_NEW_PATH} components={requireAuth(ClientNew)} />
-    <Route path={CLIENTS_PATH} components={requireAuth(ClientsIndex)} />
-    <Route path={`${CLIENTS_PATH}/:id`} components={requireAuth(ClientShow)} />
-
-
+    <Route path={CLIENT_NEW_PATH} component={requireAuth(ClientNew)} />
+    <Route path={CLIENTS_PATH} component={requireAuth(ClientsIndex)} />
+    <Route path={`${CLIENTS_PATH}/:id`} component={requireAuth(ClientShow)} />
   </Route>
 );
